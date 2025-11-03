1debf0e79a034520b3952bb97bc653fc5f5adcfa
mateusnaxx <mateus.naxx@gmail.com>
2025-11-03 12:05:31 -0300

    feat: add footer component

---
+++ app/_components/footer.tsx
@@ -0,0 +1,7 @@
+const Footer = () => {
+    return <div className="w-full bg-secondary py-6 px-5">
+        <p className="text-gray-400 text-xs font-bold opacity-75">&copy; {new Date().getFullYear()} Copyright NX Barber</p>
+    </div>;
+};
+
+export default Footer;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Footer from "./_components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
         <html lang="pt-br">
             <body className={`${inter.className} dark`}>
                 <div className="flex-1">{children}</div>
+                <Footer />
             </body>
         </html>
     );
