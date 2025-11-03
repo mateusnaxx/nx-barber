0ab810287d690a1e1db5d0707f179fe7730ce0f8
mateusnaxx <mateus.naxx@gmail.com>
2025-11-03 10:18:47 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,20 @@
 import Header from "../_components/header";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 export default async function Home() {
-  return (
-    <div>
-      <Header/>
-    </div>
-  )
+    return (
+        <div>
+            <Header />
+
+            <div className="px-5 pt-5">
+                <h2 className="text-xl font-bold">Olá, Miguel!</h2>
+                <p className="capitalize text-sm">
+                    {format(new Date(), "EEEE',' d 'de' MMMM", {
+                        locale: ptBR,
+                    })}
+                </p>
+            </div>
+        </div>
+    );
 }
