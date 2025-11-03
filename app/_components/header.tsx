eef635c45230562d0b9be65cd4060839200866b0
mateusnaxx <mateus.naxx@gmail.com>
2025-11-03 09:56:18 -0300

    feat: add initial header component

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -8,8 +8,8 @@ const Header = () => {
         <Card>
             <CardContent className="p-5 justify-between flex flex-row items-center">
                 <Image src="/logo.png" alt="NX Barber" height={22} width={120} />
-                <Button variant="outline" size="icon">
-                    <MenuIcon />
+                <Button variant="outline" size="icon" className="h-8 w-8">
+                    <MenuIcon size={18} />
                 </Button>
             </CardContent>
         </Card>
